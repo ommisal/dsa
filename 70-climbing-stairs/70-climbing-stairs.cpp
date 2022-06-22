d58444e5acaf1351e05002ad5ac1d19a2197f903
ommisal <86116898+ommisal@users.noreply.github.com>
2022-06-22 13:12:51 +0530

    Time: 2 ms (41.50%), Space: 6.2 MB (14.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,18 +4,25 @@ public:
     int climbStairs(int n) {
         
         vector<int> v(n+1,-1);
-        
-        return solve(n,v);
+        if(n==1)return 1;
+        return tabu(n,v);
+        // return memo(n,v);
         
      }
-    int solve(int n,vector<int> &v){
-        if(n<=2)return n;
-        if(v[n]!=-1){
-            return v[n];
+    // int memo(int n,vector<int> &v){
+    //     if(n<=2)return n;
+    //     if(v[n]!=-1){
+    //         return v[n];
+    //     }
+    //    return v[n] = solve(n-1,v)+solve(n-2,v);
+    //  }
+    int tabu(int n,vector<int> &v){
+        v[0]=0;
+        v[1]=1;
+        v[2]=2;
+        for(int i=3;i<=n;i++){
+            v[i] = v[i-1]+v[i-2];
         }
-       
-            return v[n] = solve(n-1,v)+solve(n-2,v);
-        
-        
+        return v[n];
     }
 };
